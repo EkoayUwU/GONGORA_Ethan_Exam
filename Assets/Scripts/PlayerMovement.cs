53545d20d3e8a49f3784a0f853ace7361ddb8a35
EkoayUwU <ethangongora24@gmail.com>
2023-04-05 14:11:24 +0200

    Exo_Done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    float horizontalValue;
+    [SerializeField] float movementSpeed;
+
+    bool canJump;
+    bool isJumping;
+    [SerializeField] float jumpForce;
+    
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        horizontalValue = Input.GetAxis("Horizontal");
+        if (horizontalValue > 0) sr.flipX = false;
+        else if (horizontalValue < 0 ) sr.flipX = true;
         
+        if (Input.GetKeyDown(KeyCode.Space) && canJump && !isJumping)
+        {
+            Jump();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalValue * Time.deltaTime * movementSpeed, rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Plateform")
+        {
+            isJumping = false;
+            canJump = true;
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!isJumping) canJump = false;
+    }
+
+    void Jump()
+    {
+        isJumping = true;
+        Debug.Log("Jump");
+        rb.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+        StartCoroutine(FallTime());
+    }
+
+    IEnumerator FallTime()
+    {
+        yield return new WaitForSeconds(0.6f);
+        rb.gravityScale = 1.75f;
+       
     }
 }
